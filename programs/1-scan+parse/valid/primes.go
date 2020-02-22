d74aaa16b7d8accbaafaa4b4d4b8ad34e1087ad8
npereira97 <43299810+npereira97@users.noreply.github.com>
2020-02-22 18:20:33 -0500

    Added final valid program (Neil)

---
+++ programs/1-scan+parse/valid/primes.go
@@ -0,0 +1,40 @@
+// (Neil Pereira)Sieve of Erasthosthenes 
+
+package main
+
+
+func printPrimes (n int){
+	if (n < 0){
+		println("n must be >= 0")
+		return
+	}
+	
+	
+	var nums []int
+	//Add all numbers less than n to list
+	for i:= 0; i < n ; i++ {
+		nums = append(nums,i)
+	}
+	
+	//Set all multiples of primes to 0
+	for i := 2; i < n ; i++{
+		k := 2
+		for j := i; j*k < n ; j++ {
+			nums[j*k] = 0
+			k++
+		}
+	}
+	
+	//All non zero entries are printed(ie the prime numbers)
+	for i := 2; i < n ; i++{
+		if nums[i] != 0{
+			println(i);
+		}
+	}	
+}
+
+func main() {
+	
+	
+	printPrimes(500);
+}
