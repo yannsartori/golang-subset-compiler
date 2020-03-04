79e8a932905433bda709ae18a754521eecb73d9c
npereira97 <43299810+npereira97@users.noreply.github.com>
2020-03-04 11:21:52 -0600

    Another delete

--- programs/1-scan+parse/invalid/neil/continueNotInLoop.go
+++
@@ -1,9 +0,0 @@
-//(Neil) Continue statements can only be present in a loop
-package main
-
-func main(){
-	
-	continue
-	
-	
-}
--- programs/1-scan+parse/invalid/neil/returnCount.go
+++
@@ -1,9 +0,0 @@
-// (Neil)The number of expressions returned by a function cannot be greater than 1
-package main
-
-func main(){
-	
-	return 1,2
-	
-	
-}
