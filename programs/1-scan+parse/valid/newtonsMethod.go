a9f8bc7a74a17966f301c2671bddff285401d63d
Yann Sartori <yannsartori@gmail.com>
2020-03-30 14:28:56 -0400

    Made programs golite compliant (reording function calls and enforcing strict type equality)

--- programs/1-scan+parse/valid/newtonsMethod.go
+++ programs/1-scan+parse/valid/newtonsMethod.go
@@ -8,44 +8,44 @@ package main;
 * statement variant as well as in the single (while) varient, indexing, type casts
 */
 type double float64
-func main() {
-	var poly []double; 
-	poly = append(poly, 1.)
-	poly = append(poly, 2.)
-	poly = append(poly, 3.)
-	poly = append(poly, 4.)
-	precision := 50
-	deriv := deriv(poly)
-	var guess double = 2.;
-	for i := 0 ; i < precision; i++ {
-		guess -= polyEvaluate(poly, guess) / polyEvaluate(deriv, guess);
-	}
-	println(guess);
-	
-}
-func deriv(poly []double) []double {
-	var result []double;
-	result = append(result, 0)
-	for i := 1; i < len(poly); i++ {
-		result = append(result, poly[i - 1] * (double) (len(poly) - i));
-	}
-	return result; 
-}
-func polyEvaluate(poly []double, x double) double {
-	var result double = .0
-	for i:= 0 ; i < len(poly); i++ {
-		result += poly[i] * power(x, len(poly) - i - 1);
-	}
-	return result;
-}
 func power(base double, exp int) double {
-	var result double = 1.0
+	var result double = double(1.0)
 	i := 0
 	for i < exp {
 		result *= base;
 		i = i + 1
 	}
 	return result
+}
+func deriv(poly []double) []double {
+	var result []double;
+	result = append(result, double(0))
+	for i := 1; i < len(poly); i++ {
+		result = append(result, poly[i - 1] * (double) (len(poly) - i));
+	}
+	return result; 
+}
+func polyEvaluate(poly []double, x double) double {
+	var result double = double(.0)
+	for i:= 0 ; i < len(poly); i++ {
+		result += poly[i] * power(x, len(poly) - i - 1);
+	}
+	return result;
+}
+func main() {
+	var poly []double; 
+	poly = append(poly, double(1.))
+	poly = append(poly, double(2.))
+	poly = append(poly, double(3.))
+	poly = append(poly, double(4.))
+	precision := double(50)
+	deriv := deriv(poly)
+	var guess double = double(2.);
+	for i := 0 ; double(i) < precision; i++ {
+		guess -= polyEvaluate(poly, guess) / polyEvaluate(deriv, guess);
+	}
+	println(guess);
+	
 }
 
 
